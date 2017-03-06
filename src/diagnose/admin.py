f88dd531cb05bb6113742cc9146a6c748e5017a9
kryvonis <Kryvonis.artem@gmail.com>
2017-03-06 13:31:04 +0200

    begining

--- src/diagnose/admin.py
+++ src/diagnose/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from src.diagnose.models import Diagnose, DiagnoseFiles
+from django.contrib import admin
+
+
+class DiagnoseFilesInline(admin.StackedInline):
+    fields = '__all__'
+
+    def get_queryset(self, request):
+        qs = super(DiagnoseFilesInline, self).get_queryset(request)
+        return qs.filter(state__gt=10)
+
 
 # Register your models here.
+class DiagnoseAdmin(admin.ModelAdmin):
+    inlines = [DiagnoseFilesInline]
+
+
+# admin.site.register(DiagnoseFiles, DiagnoseFilesAdmin)
+admin.site.register(Diagnose, DiagnoseAdmin)
--- src/diagnose/models.py
+++ src/diagnose/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class DiagnoseFiles(models.Model):
+    file = models.FileField()
+
+
 # Create your models here.
+class Diagnose(models.Model):
+    text = models.TextField()
+    attached_files = models.ManyToManyField(DiagnoseFiles)
