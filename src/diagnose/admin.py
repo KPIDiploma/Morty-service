8ba7f363174d4394fd143e7b01b374c44688c6c1
kryvonis <Kryvonis.artem@gmail.com>
2017-03-06 16:02:05 +0200

    add admin

--- src/diagnose/admin.py
+++ src/diagnose/admin.py
@@ -1,20 +1,14 @@
+from .models import Diagnose
+from .models import DiagnoseFiles
 from django.contrib import admin
-from src.diagnose.models import Diagnose, DiagnoseFiles
-from django.contrib import admin
 
 
-class DiagnoseFilesInline(admin.StackedInline):
-    fields = '__all__'
-
-    def get_queryset(self, request):
-        qs = super(DiagnoseFilesInline, self).get_queryset(request)
-        return qs.filter(state__gt=10)
+class DiagnoseFilesInline(admin.ModelAdmin):
+    pass
 
 
-# Register your models here.
 class DiagnoseAdmin(admin.ModelAdmin):
-    inlines = [DiagnoseFilesInline]
+    pass
 
-
-# admin.site.register(DiagnoseFiles, DiagnoseFilesAdmin)
+admin.site.register(DiagnoseFiles, DiagnoseFilesInline)
 admin.site.register(Diagnose, DiagnoseAdmin)
