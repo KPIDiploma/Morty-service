db0e1100e3712e43118b3139e492a42ba595094a
Kryvonis <kryvonis.artem@gmail.com>
2017-05-13 19:17:03 +0300

    added possibility to connect doctors with patients

--- src/pkg/patient/urls.py
+++ src/pkg/patient/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
 from src.pkg.patient.views import PatientViewSet
-from src.pkg.patient.views import CurrentUserView
+from src.pkg.patient.views import CurrentPatientsView
 
 
 router = DefaultRouter()
 router.register(r'v1/patients', PatientViewSet, 'patient')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    url(r'^api/v1/current-patients$', CurrentPatientsView.as_view(),
+        name='apicurrent-patients')
+]
